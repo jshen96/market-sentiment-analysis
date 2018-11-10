995def01df9045186239303951d339f744c9c44b
ysoo <ysoo@wisc.edu>
2018-11-10 14:33:27 -0600

    added real time quotes

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 var express = require("express");
 var app = express();
+const alpha = require('alphavantage')({key: '60BR33CT7KNKZ0XS'});
+
+
+alpha.data.quote('GOOGL', 'compact', 'json').then(data => {
+  console.log(data);
+})
+
 
 var PORT = process.env.PORT || 3000
 app.listen(PORT, process.env.IP, function() {

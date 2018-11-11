3ba35782ccde560e26f0c6b1716895f4e824f4bd
ysoo <ysoo@wisc.edu>
2018-11-10 23:08:57 -0600

    DONEEEEE

--- newsScraper/newsScraper.py
+++ newsScraper/newsScraper.py
@@ -1,13 +1,18 @@
 from newsapi import NewsApiClient
 import psycopg2
+from unidecode import unidecode
 
-#print('DATABASE PROGRAM STARTS')
+def xstr(s):
+    if s is None:
+        return ' '
+    return str(s)
 
 conn = None
 
 # connect to the PostgreSQL server
-conn = psycopg2.connect(port = '5432', host='ec2-18-212-49-227.compute-1.amazonaws.com',database='secret', user='power_user', password='1234')
+conn = psycopg2.connect(port = '5432', host='ec2-34-230-43-107.compute-1.amazonaws.com',database='secret', user='power_user', password='1234')
 newsapi = NewsApiClient(api_key = '4ccc6c7975b84c9fbaecc6caf24b2f0b')
+
 
 # create a cursor
 cur = conn.cursor()
@@ -23,9 +28,15 @@ for r in row:
    top_headlines = newsapi.get_everything(q=s)
    if top_headlines['status'] == 'ok':
        for art in top_headlines['articles']: 
+           # Need checking methods to make sure that strings are perfect
+           urls = xstr(art['url'])
+           titles = unidecode(xstr(art['title']))
+           description = unidecode(xstr(art['description']))
+           dob = xstr(art['publishedAt'])
+           cur.execute("INSERT INTO company_articles_news (url, headline, content, dob, cid) VALUES (%s, %s, %s, %s, %s) ON CONFLICT (url) DO NOTHING", (urls, titles, description, dob, cid))
 
-cur.execute("INSERT INTO company_articles_news (url, headline, content, dob, cid) VALUES (%s, %s, %s, %s, %s) ON CONFLICT ON CONSTRAINT url DO NOTHING", (art['url'], art['title'], art['description'], art['publishedAt'], cid))
 cur.execute('SELECT version()')
 print(cur.fetchall())
 conn.commit()
 cur.close()
+
